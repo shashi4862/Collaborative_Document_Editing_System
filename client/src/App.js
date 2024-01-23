faaf29299bffa9a64f2bf09cd95be3cc231d4129
SHASHI RANJAN <104186667+shashi4862@users.noreply.github.com>
2024-01-23 18:08:52 +0530

    init

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import TextEditor from "./TextEditor";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to={`/documents/${uuidv4()}`} />} />
+        <Route path="/documents/:id" element={<TextEditor />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
